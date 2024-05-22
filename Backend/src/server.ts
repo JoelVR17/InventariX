000c04f9ece54dd181680cbe6709f53d4db1fde9
JoelVR17 <vermudo.com@gmail.com>
2024-05-22 15:58:31 -0600

    Ready: Create and Read Products

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./router";
 import db from "./config/db";
-import swaggerUi from 'swagger-ui-express'
+import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 
 // Connect to DB
 export const connectDB = async () => {
@@ -22,12 +24,28 @@ connectDB();
 // Instance Express
 const server = express();
 
+// Allow connections
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS Error"));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
+
 // Read form data
 server.use(express.json());
+
+// See details about the request
+server.use(morgan("dev"));
 
 server.use("/api/products", router);
 
 // API Docs
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default server;
---
+++ Frontend/src/types/index.ts
@@ -0,0 +1,16 @@
+import { object, string, number, boolean, InferOutput, array } from "valibot";
+
+export const DraftProductSchema = object({
+  name: string(),
+  price: number(),
+});
+
+export const ProductSchema = object({
+  id: number(),
+  name: string(),
+  price: number(),
+  availability: boolean(),
+});
+
+export const ProductsSchema = array(ProductSchema);
+export type Product = InferOutput<typeof ProductSchema>;
